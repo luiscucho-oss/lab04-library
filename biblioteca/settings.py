fd7d4be99e6e454b363cfadde610772591094511
luis <luis.cucho@tecsup.edu.pe>
2026-09-17 21:03:00 -0500

    Configurar rutas de medios en settings y urls

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
